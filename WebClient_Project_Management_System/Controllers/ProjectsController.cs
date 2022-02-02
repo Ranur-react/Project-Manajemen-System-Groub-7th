afcd72d4b0571736062ba3f8f739003e76064838
Erwin Leo Sinaga <erwindev.tech@gmail.com>
2022-02-02 20:39:07 +0700

    return http status on post project

--- WebClient_Project_Management_System/Controllers/ProjectsController.cs
+++ WebClient_Project_Management_System/Controllers/ProjectsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using WebClient_Project_Management_System.Base;
 using WebClient_Project_Management_System.Models;
@@ -46,7 +47,7 @@ namespace WebClient_Project_Management_System.Controllers
         public IActionResult Add (Project entity)
         {
             var result =  projectRepository.Post(entity);
-            return RedirectToAction("index", "Projects");
+            return RedirectToAction("index", "Projects",new { status= result });
         }
 
     }
